b14f3eef35643ed6de31ca2c46bba8c91cadc7b5
Jean-claude Lhote <jean-claude.lhote@sesamath.net>
2022-10-03 19:02:48 +0200

    version qui fonctionne

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,14 +1,9 @@
 import { answer } from '#lib/counter'
 import { expect } from 'chai'
-// import { response } from '#lib/question'
 
 describe('mocha runs', () => {
   it('correctly', () => true)
   it('resolve imports with aliases', () => {
     expect(answer()).to.equals(42)
   })
- /* it('resolve imports *.js with aliases', () => {
-    expect(response().to.equals(24))
-  })
-  */
 })
