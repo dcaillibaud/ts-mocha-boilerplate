c2b98b546cc8cc00ccf71272c2a2ba64b1e62ac8
Jean-claude Lhote <jean-claude.lhote@sesamath.net>
2022-10-03 18:40:21 +0200

    Le pnpm test passe ici

--- test/main.test.js
+++
@@ -1,9 +0,0 @@
-import { expect } from 'chai'
-import { answer } from 'lib/counter'
-
-describe('mocha runs', () => {
-  it('correctly', () => true)
-  it('resolve imports with aliases', () => {
-    expect(answer()).to.equals(42)
-  })
-})
