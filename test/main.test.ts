63b9118c452ca06f2b421f2f41c8edbd345d4a6f
Daniel Caillibaud <daniel.caillibaud@sesamath.net>
2022-10-04 14:55:40 +0200

    OK with ts-node 10 (need esm loader + .js extension in test files)

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai'
 
-import { answer } from 'lib/counter'
-import { response } from 'lib/question'
+import { answer } from 'lib/counter.js'
+import { response } from 'lib/question.js'
 
 describe('mocha runs', () => {
   it('correctly', () => true)
